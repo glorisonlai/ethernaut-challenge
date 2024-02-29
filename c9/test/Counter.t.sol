8266367ff28842352ff77e4bc74e37a3f3a455f2
lai.glorison <lai.glorison@gmail.com>
2024-02-29 17:45:06 +1100

    c9

--- c9/test/Counter.t.sol
+++ c9/test/Counter.t.sol
@@ -2,23 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/King.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    King public king;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        king = new King();
     }
-
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    
+    function test() public {
+        address kingAddy = address(king);
+        
     }
 }
