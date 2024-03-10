0c7b756ddb5cfbfefbab62df995e0961602eb217
lai.glorison <lai.glorison@gmail.com>
2024-03-11 01:53:15 +1100

    c22

--- c22/src/Dex.sol
+++ c22/src/Dex.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "openzeppelin-contracts-08/token/ERC20/IERC20.sol";
-import "openzeppelin-contracts-08/token/ERC20/ERC20.sol";
-import 'openzeppelin-contracts-08/access/Ownable.sol';
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import 'openzeppelin-contracts/contracts/access/Ownable.sol';
 
-contract Dex is Ownable {
+abstract contract Dex is Ownable {
   address public token1;
   address public token2;
   constructor() {}
